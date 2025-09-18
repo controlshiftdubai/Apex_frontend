1d40beffe29d9484b4e2d4116df5066bd7bea4bb
NAWNEET KUAMR GUPTA <159549550+nawneetgupta7479@users.noreply.github.com>
2025-09-18 14:58:23 +0530

    changes

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -4,7 +4,7 @@ import { FlipText } from "@/components/magicui/flip-text";
 
 export default function Footer() {
   return (
-    <footer className="bg-[#f6f3f1] text-black min-h-screen flex items-end relative">
+    <footer className="bg-white text-black min-h-screen flex items-end relative">
       <div className="absolute inset-0 flex items-center flex-col justify-center px-4">
 <motion.div
   initial="hidden"
@@ -13,7 +13,7 @@ export default function Footer() {
   className="space-y-2"
 >
   <FlipText
-    className="font-light text-gray-900 leading-tight text-center text-2xl sm:text-3xl md:text-4xl lg:text-[48px] max-w-5xl"
+    className="font-light text-black leading-tight text-center text-2xl sm:text-3xl md:text-5xl lg:text-5xl max-w-5xl"
     duration={0.01}
     delayMultiple={0.01}
   >
@@ -21,7 +21,7 @@ export default function Footer() {
   </FlipText>
 
   <FlipText
-    className="font-light text-gray-900 leading-tight text-center text-2xl sm:text-3xl md:text-4xl lg:text-[48px] max-w-5xl"
+    className="font-light text-black leading-tight text-center text-2xl sm:text-3xl md:text-5xl lg:text-5xl max-w-5xl"
     duration={0.01}
     delayMultiple={0.01}
     startDelay={0.4} // 👈 delay until first line completes
@@ -38,7 +38,7 @@ export default function Footer() {
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true, amount: 0.5 }}
           transition={{ duration: 1.8, ease: "easeOut", delay: 1.8 }}
-          className="link-highlight !cursor-pointer px-6 link-highlight-yellow relative inline-block text-2xl text-black my-12"
+          className="link-highlight !cursor-pointer px-6 link-highlight-yellow relative inline-block text-[25px] text-black my-12"
         >
           See your  projects today
         </motion.p>
