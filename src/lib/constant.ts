e3e8baccacfb1c8828fce9a33b4866f2f32bdbb7
Asjad Nasim <asjadnasim@gmail.com>
2025-10-15 21:55:03 +0530

    fix bug

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,8 +1,8 @@
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
-// export const API_BASE_URL =
-// process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080";
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080";
+// export const API_BASE_URL = "http://localhost:8080";
 
 export const APP_URL =
   process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
